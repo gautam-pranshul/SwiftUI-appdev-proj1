db24891104cddaff08d9a3df49181d3e7ce5a3af
Pranshul <pranshulgautam2702@gmail.com>
2024-08-24 21:54:53 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Pranshul Gautam on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
